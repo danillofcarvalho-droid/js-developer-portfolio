045ca5b68fd26daf4ae692c1f098fbf9b6dd1fd6
danillofcarvalho-droid <danillofcarvalho@gmail.com>
2026-01-08 12:33:39 -0300

    alterando o json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+  const response = await fetch(`data/profile.json?v=${Date.now()}`)
+  const profileData = await response.json()
+  return profileData
 }
+
